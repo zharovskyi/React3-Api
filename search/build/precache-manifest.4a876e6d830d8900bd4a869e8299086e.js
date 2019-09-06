401c509412f90e9c2105c9c0631b20db433bdab9
Oleh Zharovskyi <olegzhora11@gmail.com>
2019-09-06 19:01:28 +0300

    add build new build some

---
+++ search/build/precache-manifest.4a876e6d830d8900bd4a869e8299086e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b11726912d2c653a46e33bf395cdeae",
+    "url": "/React3-Api/search/build/index.html"
+  },
+  {
+    "revision": "89172779d4b040df234b",
+    "url": "/React3-Api/search/build/static/css/main.cda62dd1.chunk.css"
+  },
+  {
+    "revision": "f777359d7b6d266f4827",
+    "url": "/React3-Api/search/build/static/js/2.56059f80.chunk.js"
+  },
+  {
+    "revision": "89172779d4b040df234b",
+    "url": "/React3-Api/search/build/static/js/main.0afe83b2.chunk.js"
+  },
+  {
+    "revision": "b66e087e678e4b7efd9f",
+    "url": "/React3-Api/search/build/static/js/runtime~main.72bc5529.js"
+  }
+]);
